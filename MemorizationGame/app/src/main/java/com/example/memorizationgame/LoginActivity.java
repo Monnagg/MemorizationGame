9bd833cd59cae7c87e9e2a21baec9c6c61dabf24
Monnagg <jiangmenghua1992@gmail.com>
2022-05-14 23:30:35 -0400

    Version03

--- MemorizationGame/app/src/main/java/com/example/memorizationgame/LoginActivity.java
+++ MemorizationGame/app/src/main/java/com/example/memorizationgame/LoginActivity.java
@@ -2,26 +2,21 @@ package com.example.memorizationgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.io.InputStreamReader;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,65 +24,45 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        //setting music
-        final Intent serviceIntent = new Intent(LoginActivity.this,MusicService.class);
-        ImageButton musicPlayer = (ImageButton)findViewById(R.id.sound);
-        if(MusicService.isplay == false){
-            musicPlayer.setImageResource(R.drawable.start);
-        }
-        musicPlayer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(MusicService.isplay == false){
-                    startService(serviceIntent);
-                    musicPlayer.setImageResource(R.drawable.stop);
-                }else {
-                    stopService(serviceIntent);
-                    musicPlayer.setImageResource(R.drawable.start);
-                }
-            }
-        });
-
-        String name = ((EditText) findViewById(R.id.nameinput)).getText().toString();
-        String password = ((EditText) findViewById(R.id.passinput)).getText().toString();
-        Button login = (Button) findViewById(R.id.loginbutton);
-
-
-
-        //setting cancel button
+        //login
+        /*
         Button cancel = (Button)findViewById(R.id.cancelbutton);
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
 
-        //setting question mark
-        ImageButton questionmark = (ImageButton) findViewById(R.id.questionmark2);
-        questionmark.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showDialog(view );
-            }
+         */
+        /*
+        Button login = (Button)findViewById(R.id.loginbutton);
+        login.setOnClickListener(new View.OnClickListener() {
+            InputStream is = this.getResources().openRawResource(R.drawable.allUsers);
         });
+
+         */
+
     }
 
-    public  void showDialog(View view){
-        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(this);
-        builder.setTitle("Instruction");
-        builder.setMessage("choose the right shape that you have already seen");
-        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+    public void signIn(View view) throws IOException {
+        String username = ((EditText) findViewById(R.id.nameinput)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passinput)).getText().toString();
+        InputStream is = this.getResources().openRawResource(R.raw.all_users);
+        FileIO fio = new FileIO(is);
+        if (fio.match(username,password)){
+            UerAccount user = fio.getUser(username);
+            Intent intent = new Intent(LoginActivity.this,AccountActivity.class);
+            intent.putExtra("user",user);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(LoginActivity.this, "The entered username and password does not match records", Toast.LENGTH_SHORT).show();
+        }
 
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
     }
 
-
+    public void cancel(View view){
+        finish();
+    }
 }
